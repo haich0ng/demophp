0b316dae8daf910af90d0523a8b433037771db0b
haich0ng <shanghaihaze@hotmail.com>
2021-10-11 10:40:34 +0100

    bug

--- addusers.php
+++ addusers.php
@@ -11,16 +11,16 @@ switch($_POST["role"]){
 		$role=2;
 		break;
 }
-$stmt = $conn->prepare("INSERT INTO TblUser (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
 
-$stmt->bindParam(':forename', $_POST[“forename”]);
-$stmt->bindParam(':surname', $_POST[“surname”]);
-$stmt->bindParam(':house', $_POST[“house”]);
-$stmt->bindParam(':year', $_POST[“year”]);
-$stmt->bindParam(':password', $_POST[“passwd”]);
-$stmt->bindParam(':gender', $_POST[“gender”]);
+$stmt->bindParam(':forename', $_POST["forename"]);
+$stmt->bindParam(':surname', $_POST["surname"]);
+$stmt->bindParam(':house', $_POST["house"]);
+$stmt->bindParam(':year', $_POST["year"]);
+$stmt->bindParam(':password', $_POST["passwd"]);
+$stmt->bindParam(':gender', $_POST["gender"]);
 $stmt->bindParam(':role', $role);
 $stmt->execute();
 $conn=null;
-
+header('Location: user.php');
 ?>
