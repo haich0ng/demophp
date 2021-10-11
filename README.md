39822a02248c6309beb53a7cabfe3f378186aa1a
haich0ng <shanghaihaze@hotmail.com>
2021-10-11 09:47:08 +0100

    test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demophp
+Hello world 
---
+++ addusers.php
@@ -0,0 +1,9 @@
+<?php
+echo $_POST["gender"]."<br>";
+echo $_POST["forename"]."<br>";
+echo $_POST["surname"]."<br>";
+echo $_POST["house"]."<br>";
+echo $_POST["year"]."<br>";
+echo $_POST["passwd"]."<br>";
+echo $_POST["role"]."<br>";
+?>
