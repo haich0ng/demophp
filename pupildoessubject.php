ae570df94eb86494049fc07dd5c5f47d4ff8d36c
haich0ng <shanghaihaze@hotmail.com>
2021-11-01 10:40:19 +0000

    1

--- pupildoessubject.php
+++ pupildoessubject.php
@@ -1,12 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+</html>
 <select name = "student">
 <?php
 include_once("connection.php");
-$stmt = $somm->prepare("SELECT * FROM Tblusers WHERE Role = 0 ORDER BY Surname ASC");
+$stmt = $conn->prepare("SELECT * FROM Tblusers WHERE Role = 0 ORDER BY Surname ASC");
 $stmt -> execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
 	echo("<option value=".$row["UserID"].">".$row["Surname"].", ".$row["Forename"]."</option>");
 }
 ?>
-</select
-?>
+</select>
